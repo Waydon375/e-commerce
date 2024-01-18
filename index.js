5bb3c29f6d34aec42410e8eac9a1dbadb453d248
Waydon375 <saalwaydon@gmail.com>
2024-01-18 12:46:59 +0200

    jordan2.png removed for jordan2.jpeg

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
 
+const product = [
+    {
+        id:1,
+        title:"Air Force",
+        price: R2220,
+        colors:[
+            {
+            code:"black",
+            img: './img/air.png',
+        },
+        {
+            code:"darkblue",
+            img:"./img/air2.png",
+        }
+    ]
+    },
+
+    {
+        id: 2,
+        title: Jordan,
+        price:2800,
+        colors:[{
+            code:
+        }]
+    },
+]
+
 menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
         wrapper.style.transform = `translateX(${-100 * index}vw)`;
