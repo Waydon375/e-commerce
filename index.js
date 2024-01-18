bb448b8d81f23e98f484c9eb9fd62314b651b095
Waydon375 <saalwaydon@gmail.com>
2024-01-18 13:05:11 +0200

    changed the  pricing color

--- index.js
+++ index.js
@@ -1,32 +1,63 @@
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
 
-const product = [
-    {
-        id:1,
-        title:"Air Force",
-        price: R2220,
-        colors:[
-            {
-            code:"black",
-            img: './img/air.png',
-        },
-        {
-            code:"darkblue",
-            img:"./img/air2.png",
-        }
-    ]
-    },
+// const product = [
+//     {
+//         id:1,
+//         title:"Air Force",
+//         price: R2220,
+//         colors:[
+//             {
+//             code:"black",
+//             img: './img/air.png',
+//         },
+//         {
+//             code:"darkblue",
+//             img:"./img/air2.png",
+//         }
+//     ]
+//     },
 
-    {
-        id: 2,
-        title: Jordan,
-        price:2800,
-        colors:[{
-            code:
-        }]
-    },
-]
+//     {
+//         id: 2,
+//         title: "Air Jordan",
+//         price:2800,
+//         colors:[{
+//             code: "grey/black/orange",
+//             img: "./img/jordan.png",
+//         },
+//         {
+//             code:'grey/black',
+//             img: "./img/jordan2.jpeg",
+//         }
+//     ]
+//     },
+//     {
+//         id:3,
+//         title:"Blazer",
+//         price:R3500,
+//         color: [
+//             {
+//                 code:'white/black',
+//                 img:'./img/blazer.png',
+//         },
+//         {
+//             code:'green/white',
+//             img:'./img/blazer2.png',
+//         }
+//     ]
+//     },
+//     {
+//         id: 4,
+//         title:'Crater',
+//         price:R2400,
+//         colors:[{
+//             code:'black/white',
+//             img:'./img/crater.png'
+//         }]
+//     }
+
+// ]
 
 menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
