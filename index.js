854c7d66a4a8128c94fbfe2f77e7acc4f566e2b3
Waydon375 <saalwaydon@gmail.com>
2024-01-24 13:42:07 +0200

    #

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ const currentProductImg = document.querySelector(".productImg");
 const currentProductTitle = document.querySelector(".productTitle");
 const currentProductPrice = document.querySelector(".productPrice");
 const currentProductColors = document.querySelectorAll(".color"); 
-const currentProductSize = document.querySelectorAll(".size"); 
+const currentProductSizes = document.querySelectorAll(".size"); 
 
 menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
@@ -106,4 +106,27 @@ currentProductColors.forEach((color ,index)=>{
     color.addEventListener('click', ()=>{
         currentProductImg.src = chosenProduct.colors[index].img
     })
+});
+
+currentProductSizes.forEach ((size, index)=>{
+    size.addEventListener("click", ()=>{
+        currentProductSizes.forEach(size=>{
+        size.style.backgroundColor = 'white'
+        size.style.color = 'black'
+        })
+        size.style.backgroundColor = 'black'
+        size.style.color = 'white'
+    });
+});
+
+const productButton = document.querySelector('.productButton');
+const payment = document.querySelector('.payment');
+const close = document.querySelector('.close');
+
+productButton.addEventListener('click', ()=>{
+    payment.style.display = 'flex'
+})
+
+close.addEventListener('click' ,() =>{
+    payment.style.display='none'
 })
